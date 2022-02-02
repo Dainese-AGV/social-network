e4a34999935496e505f0ee1a3cc82b2065b0d1ed
Dainese-AGV <Dtarankevich@mail.ru>
2022-02-02 23:19:58 +0300

    lesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+    <header className="header">
+        <img src="https://www.nicepng.com/png/full/133-1336434_blue-header-image-blue-header-png.png" alt="page"></img>
+    </header>
+    )
+    }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+
+
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://cdn-icons-png.flaticon.com/512/147/147144.png" alt="page"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
